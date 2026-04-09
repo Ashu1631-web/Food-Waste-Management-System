6fd65d9c96d3c7513dd46787f76ac5379143fd65
Ashish Mehara <viratmehara232@gmail.com>
2026-04-10 00:01:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Food Waste Management", layout="wide")
 st.markdown("""
 <style>
 .stApp {
-    background-image: url("https://images.unsplash.com/photo-1490645935967-10de6ba17061");
+    background-image: url("https://plus.unsplash.com/premium_photo-1673108852141-e8c3c22a4a22?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
     background-size: cover;
 }
 .login-box {
